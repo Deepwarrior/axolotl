ac3d08355d692c548015b343758ba4b20dd28f9c
2321 <wqewq@12w.ru>
2017-07-09 18:53:42 +0300

    v 1.2 Add counter for messages. Add alarm in debug chat when task is expired.

--- axol/Players.py
+++ axol/Players.py
@@ -5,13 +5,15 @@ class Player_state :
     def __init__(self, user):
         self.user = user
         self.task_status = 0 #0 - without task; 1 - in progress; 2 - failed.
-        self.last_task = 0 #time when last task had taken.
+        self.last_task_time = 0 #time when last task had taken.
+        self.last_task_mssg = 0 #message when task had taken.
         self.task_completed = 0
         self.task = None
+        self.informed = False
 
 def to_string(self):
     res = str(self.user.first_name) + ' ' + str(self.user.last_name) + ' @' + str(self.user.username) + '\n'
-    secs = time.time() - self.last_task
+    secs = time.time() - self.last_task_time
     res += 'Time: ' + str(secs // 3600) +'h ' + str(secs // 60 % 60) +'m ' + str(secs // 1 % 60) +'s\n'
     res += self.task.text
     return res
--- axol/axol.py
+++ axol/axol.py
@@ -10,6 +10,10 @@ import json
 bot = telebot.TeleBot(config.token)
 active_players = []
 
+vip_chat_id = -1001107497089 #now it`s debug chat replace with: -1001090074308  #check.
+debug_chat_id = -1001107497089
+
+
 def jsonDefault(object):
     return object.__dict__
 
@@ -20,17 +24,6 @@ class obj(object):
                setattr(self, a, [obj(x) if isinstance(x, dict) else x for x in b])
             else:
                setattr(self, a, obj(b) if isinstance(b, dict) else b)
-
-@bot.message_handler(content_types=["sticker"])
-def sticker_parsing(message): 
-    if message.from_user.username == "sverhmassivnaya":
-        if message.sticker.file_id == 'CAADAgADHgAD6gKUEl9xLyPpAAFHBgI':
-            bot.send_message(message.chat.id, "Погоди, сейчас выдам Дипа")
-    elif message.from_user.username == "random_answer":
-        for w in config.hi_stickers[:]:
-            if message.sticker.file_id == w:
-                bot.send_message(message.chat.id, random.choice(config.hi_citrus))
-    bot.send_message(message.chat.id, message.sticker.file_id)
 
 #find and append players
 def findplayer(user):
@@ -44,23 +37,26 @@ def findplayer(user):
 #collect players and give them tasks
 @bot.message_handler(commands=["get_task"])
 def task_send(message):
-    player = findplayer(message.from_user)
-    if time.time() -  player.last_task > config.seconds_in_day:
-        player.task_status = 0
-    if player.task_status == 1:
-        bot.send_message(message.chat.id, "ТЫ УЖЕ ЧТО-ТО ДЕЛАЕШЬ!", reply_to_message_id = message.message_id)
-    elif player.task_status == 2:
-        bot.send_message(message.chat.id, "ТЫ УЖЕ НЕ СМОГ!", reply_to_message_id = message.message_id)
-    elif player.task_status == 0:
-        if time.time() -  player.last_task < config.seconds_in_day:
-             bot.send_message(message.chat.id, "НОВОЕ ЗАДАНИЕ БУДЕТ ЗАВТРА!", reply_to_message_id = message.message_id)
-        else:
-            player.task_status = 1
-            player.last_task = time.time()
-            task = random.choice(config.tasks)
-            bot.send_sticker(message.chat.id, task[0])
-            bot.send_message(message.chat.id, task[1])
-            player.task = tasks.Task(*task)
+    if message.chat.id != vip_chat_id and  message.chat.id != debug_chat_id:
+        bot.send_message(message.chat.id, "ПО ЛИЧКАМ ШУШУКАЕТЕСЬ? НЕ ТОТ ЧЯТИК!", reply_to_message_id = message.message_id) #check!!! # IMPORTANT
+    else:
+        player = findplayer(message.from_user)
+        if time.time() -  player.last_task_time > config.seconds_in_day:
+            player.task_status = 0
+        if player.task_status == 1:
+            bot.send_message(message.chat.id, "ТЫ УЖЕ ЧТО-ТО ДЕЛАЕШЬ!", reply_to_message_id = message.message_id)
+        elif player.task_status == 2:
+            bot.send_message(message.chat.id, "ТЫ УЖЕ НЕ СМОГ!", reply_to_message_id = message.message_id)
+        elif player.task_status == 0:
+            if time.time() -  player.last_task_time < config.seconds_in_day:
+                bot.send_message(message.chat.id, "НОВОЕ ЗАДАНИЕ БУДЕТ ЗАВТРА!", reply_to_message_id = message.message_id)
+            else:
+                player.task_status = 1
+                player.last_task_time = time.time()
+                task = random.choice(config.tasks)
+                bot.send_sticker(message.chat.id, task[0])
+                bot.send_message(message.chat.id, task[1])
+                player.task = tasks.Task(*task)
 
 # root command. See all players with tasks.
 @bot.message_handler(commands=["all_tasks"])
@@ -81,19 +77,62 @@ def task_send(message):
 @bot.message_handler(commands=["help"])
 def task_send(message):
     bot.send_message(message.chat.id, random.choice(config.help_list), reply_to_message_id = message.message_id)
+    bot.send_message(message.chat.id, message.message_id)
 
 @bot.message_handler(commands=["donate"])
 def task_send(message):
     bot.send_message(message.chat.id, random.choice(config.donate_list), reply_to_message_id = message.message_id)
 
-@bot.message_handler(commands=["roll"])
+@bot.message_handler(commands=["backup"])
 def task_send(message):
     f = open('players.json', 'w')
     json.dump(active_players, f, default=jsonDefault)
-    bot.send_message(message.chat.id, random.randint(1,6), reply_to_message_id = message.message_id)
+    f.close()
+
+@bot.message_handler(content_types=["sticker"])
+def sticker_parsing(message):
+    for w in active_players[:]:
+        if w.informed == False:
+            if w.task.time:
+                if time.time() - w.last_task_time >  w.task.time * 3600:
+                    bot.send_message(debug_chat_id, players.to_string(w) + '\nВремя задания истекло! Оцените!')
+                    w.informed = True
+            if w.task.messages:
+                if message.message_id - w.last_task_mssg >  w.task.messages:
+                    bot.send_message(debug_chat_id, players.to_string(w) + '\nВсе сообщения написаны! Оцените!')
+                    w.informed = True
+
+    if message.from_user.username == "sverhmassivnaya":
+        if message.sticker.file_id == 'CAADAgADHgAD6gKUEl9xLyPpAAFHBgI':
+            bot.send_message(message.chat.id, "Погоди, сейчас выдам Дипа", reply_to_message_id = message.message_id)
+    elif message.from_user.username == "random_answer":
+        for w in config.hi_stickers[:]:
+            if message.sticker.file_id == w:
+                bot.send_message(message.chat.id, random.choice(config.hi_citrus), reply_to_message_id = message.message_id)
+    if message.sticker.file_id == 'CAADAgADpgEAAmDrzgNSIT8rlE3K0AI':
+        for w in config.root[:]:
+            if message.from_user.username == w:
+                player = findplayer(message.reply_to_message.from_user)
+                if player.task:
+                    if player.task_status == 0:
+                        player.task_status = 1
+                        player.task_completed -= 1
+                        bot.send_message(message.chat.id, "НЕ, АДМИНАМ НЕ НРАВИТСЯ")
+    else: bot.send_message(message.chat.id, message.sticker.file_id, reply_to_message_id = message.message_id) #del
 
 @bot.message_handler(content_types=["text"])
 def message_parsing(message):
+    for w in active_players[:]:
+        if w.informed == False:
+            if w.task.time:
+                if time.time() - w.last_task_time >  w.task.time * 3600:
+                    bot.send_message(debug_chat_id, players.to_string(w) + '\nВремя задания истекло! Оцените!')
+                    w.informed = True
+            if w.task.messages:
+                if message.message_id - w.last_task_mssg >  w.task.messages:
+                    bot.send_message(debug_chat_id, players.to_string(w) + '\nВсе сообщения написаны! Оцените!')
+                    w.informed = True
+
     if message.text == 'МОЛОДЕЦ!':
         if message.reply_to_message:
             for w in config.root[:]:
@@ -103,7 +142,7 @@ def message_parsing(message):
                         player.task_status = 0
                         player.task_completed += 1
                         bot.send_message(message.chat.id, "ЗАДАНИЕ ВЫПОЛНЕНО!\nВСЕГО СДЕЛАНО " + str(player.task_completed) + " ЗАДАНИЙ", reply_to_message_id = message.reply_to_message.message_id)
-    if message.text == 'ТЫ ДУРА?':
+    elif message.text == 'ТЫ ДУРА?':
         if message.reply_to_message:
             for w in config.root[:]:
                 if message.from_user.username == w: 
