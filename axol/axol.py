6c87a76664dbfc13e1a8f78c2c2f6eb0417f05f0
Deepwarrior <piskun_ya@mail.ru>
2017-11-22 22:11:23 +0200

    fix sarastie last

--- axol/axol.py
+++ axol/axol.py
@@ -259,8 +259,10 @@ def pozor(message):
 @bot.message_handler(commands=["top_sarastie"])
 def sarasti(message):
     text = "ТОП САРАСТИ:\n-1. АРУЛУТ\n1. САРАСТИ\n2. САРАСТИШЕЧКА\n3. РАСТИШИШКА\n4. s a r A S I S k a\n5. СИСЕНИКА"
-    bot.send_voice(message.chat.id, 'AwADAgAD0QADNxYpSFb3d6KS2tHAAg', caption=text)
-    bot.send_message(message.chat.id, text)
+    try:
+        bot.send_voice(message.chat.id, 'AwADAgAD0QADNxYpSFb3d6KS2tHAAg', caption=text)
+    except telebot.apihelper.ApiException:
+        bot.send_message(message.chat.id, text)
 
 @bot.message_handler(commands=["my_task"])
 def task_status(message):
