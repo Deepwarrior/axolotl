bca0a37a38adc82dbf1ec9a779f94c2507fc3740
Unknown <chickensoup@bk.ru>
2018-02-14 01:44:53 +0400

    kick kotya and tadeush

--- axol/axol.py
+++ axol/axol.py
@@ -568,28 +568,33 @@ def love_set(message):
     players_in_love = []
     for player in active_players:
         if player.islove:
-            players_in_love.append(player)
-    random.shuffle(players_in_love)
-    lovers = len(players_in_love)
-    for i in range(lovers):
-        player = players_in_love[i]
-        pair = players_in_love[(i+1) % lovers]
-        player.pair = ""
-        if player.user.first_name:
-            player.pair += str(pair.user.first_name) + '\t'
-        if player.user.last_name:
-            player.pair += str(pair.user.last_name) + '\t'
-        if player.user.username:
-            player.pair += '@' + str(pair.user.username) + '\t'
-        player.love_task = random.choice(config.love_tasks)
-        try:
-            bot.send_message(player.user.id, 'АКСОЛОТЛЬ-КУПИДОН НАУДАЧУ ЗАПУСТИЛ'
-                                            ' СВОЮ СТРЕЛУ. ТВОЯ ВТОРАЯ ПОЛОВИНКА '
-                                      + player.pair + ' УЖЕ ЖДЁТ ОТ ТЕБЯ ЗНАКА ВНИМАНИЯ!')
-            bot.send_sticker(player.user.id, 'CAADAgADUgADsjRGHr5CgRYMzRQNAg')
-            bot.send_message(player.user.id, player.love_task + ' \u2764 \u2764 \u2764')
-        except telebot.apihelper.ApiException:
-            continue
+            try:
+                status = bot.get_chat_member(message.chat.id, player.user.id)
+            except telebot.apihelper.ApiException:
+                continue
+            if status and status.status in ["member", "creator", "administrator"] and not player.user.username == "rakon_bot":
+                players_in_love.append(player)
+        random.shuffle(players_in_love)
+        lovers = len(players_in_love)
+        for i in range(lovers):
+            player = players_in_love[i]
+            pair = players_in_love[(i+1) % lovers]
+            player.pair = ""
+            if player.user.first_name:
+                player.pair += str(pair.user.first_name) + '\t'
+            if player.user.last_name:
+                player.pair += str(pair.user.last_name) + '\t'
+            if player.user.username:
+                player.pair += '@' + str(pair.user.username) + '\t'
+            player.love_task = random.choice(config.love_tasks)
+            try:
+                bot.send_message(player.user.id, 'АКСОЛОТЛЬ-КУПИДОН НАУДАЧУ ЗАПУСТИЛ'
+                                                ' СВОЮ СТРЕЛУ. ТВОЯ ВТОРАЯ ПОЛОВИНКА '
+                                          + player.pair + ' УЖЕ ЖДЁТ ОТ ТЕБЯ ЗНАКА ВНИМАНИЯ!')
+                bot.send_sticker(player.user.id, 'CAADAgADUgADsjRGHr5CgRYMzRQNAg')
+                bot.send_message(player.user.id, player.love_task + ' \u2764 \u2764 \u2764')
+            except telebot.apihelper.ApiException:
+                continue
 
 @bot.message_handler(commands=["love"])
 def love(message):
@@ -597,13 +602,19 @@ def love(message):
     if message.from_user.username in config.root:
         for player in active_players:
             if player.islove:
-                if player.user.first_name:
-                    answer += str(player.user.first_name) + '\t'
-                if player.user.last_name:
-                    answer += str(player.user.last_name) + '\t'
-                if player.user.username:
-                    answer += '@' + str(player.user.username) + '.\t'
-                answer += '\n'
+                try:
+                    status = bot.get_chat_member(message.chat.id, player.user.id)
+                except telebot.apihelper.ApiException:
+                    continue
+                if status and status.status in ["member", "creator",
+                                            "administrator"] and not player.user.username == "rakon_bot":
+                    if player.user.first_name:
+                        answer += str(player.user.first_name) + '\t'
+                    if player.user.last_name:
+                        answer += str(player.user.last_name) + '\t'
+                    if player.user.username:
+                        answer += '@' + str(player.user.username) + '.\t'
+                    answer += '\n'
         bot.send_message(message.chat.id, answer)
 
 @bot.message_handler(commands=["love_all"])
