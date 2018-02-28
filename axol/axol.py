7dbefcd1cd08a64b4e534bbe450a104882ac40a7
Deepwarrior <piskun_ya@mail.ru>
2018-02-28 08:43:52 +0200

    Zrena update

--- axol/axol.py
+++ axol/axol.py
@@ -38,13 +38,13 @@ def zrena_timers_init():
     tim = (day + 55 * 60 - hours * 3600 - mins * 60 - sec) % day
     timer = Timer(tim, zrena)
     timer.start()
-    tim = (day + 30 * 60 + 9 * 3600 - hours * 3600 - mins * 60 - sec) % day
+    tim = (day + 20 * 60 + 9 * 3600 - hours * 3600 - mins * 60 - sec) % day
     timer = Timer(tim, zrena)
     timer.start()
-    tim = (day + 20 * 60 + 20 * 3600 - hours * 3600 - mins * 60 - sec) % day
+    tim = (day + 5 * 60 + 20 * 3600 - hours * 3600 - mins * 60 - sec) % day
     timer = Timer(tim, zrena)
     timer.start()
-    tim = (day + 25 * 60 + 14 * 3600 - hours * 3600 - mins * 60 - sec) % day
+    tim = (day + 40 * 60 + 14 * 3600 - hours * 3600 - mins * 60 - sec) % day
     timer = Timer(tim, zrena)
     timer.start()
 
