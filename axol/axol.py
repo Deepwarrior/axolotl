baf4aed7a1a3739ee4780ac7971ee93426a78a18
Alexey <piskun_ya@mail.ru>
2018-07-10 09:47:21 +0300

    fix typo

--- axol/axol.py
+++ axol/axol.py
@@ -130,7 +130,7 @@ def level_up():
     level += 1
     print(level)
     if level <= len(config.questions)-1:
-        question = "ДЕРЖИ ЕЩЁ ВОПРОС:\n" + config.questions[level]
+        question = "ДЕРЖИ ВОПРОС:\n" + config.questions[level]
         bot.send_message(vip_chat_id, question)
     elif level == len(config.questions):
         bot.send_message(vip_chat_id, "ТЫ ПОДЕБИЛ")
