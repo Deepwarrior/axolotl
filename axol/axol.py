321fb9c92fe25e0e2f92ed23c578ada4965d30b7
Unknown <chickensoup@bk.ru>
2018-12-07 01:53:03 +0400

    eallo moved to superreactions

--- axol/axol.py
+++ axol/axol.py
@@ -1725,12 +1725,35 @@ def dura_win(reaction, message):
 def why_yellow(reaction, message):
     bot.forward_message(message.chat.id, debug_chat_id, 57759)
 
+
+def grammar_check(reaction, message):
+    if not message.chat.id == message.from_user.id:
+        text = message.text.upper()
+        rnd = random.randint(0, 3)
+        for word in config.grammar_nazi_dictionary.keys():
+            if word in text:
+                if not rnd:
+                    try:
+                        bot.restrict_chat_member(message.chat.id, message.from_user.id, 1 * 60 * 60, False, False, False,
+                                                 False)
+                        bot.send_message(message.chat.id, "ПОДУМОЙ НАД СВОИМ ПОВЕДЕНИЕМ.",
+                                         reply_to_message_id=message.message_id)
+                    except telebot.apihelper.ApiException:
+                        bot.send_message(message.chat.id, "Я Б ТЕБЯ ЗАБАНИЛ, ДА ЛАПКИ МАРАТЬ НЕОХОТА.",
+                                         reply_to_message_id=message.message_id)
+                else:
+                    str = random.choice(config.grammar_nazi_explanation)
+                    answer = str[0] + config.grammar_nazi_dictionary[word] + str[1]
+                    bot.send_message(message.chat.id, answer, reply_to_message_id=message.message_id)
+
+
 reaction_funcs = {"task_rework": task_rework, "task_fail": task_fail, "task_complete": task_complete,
                   "task_extra": task_extra, "natalka": natalka, "kick_bots": kick_bots, "kick_lyuds": kick_lyuds,
                   "mem_react": mem_react, "anti_task": anti_task, "set_admin": set_admin, "whois": whois,
                   "stop_natalka": stop_natalka, "kick_citrus": kick_citrus, "kick_rels": kick_rels,
                   "kick_misha": kick_misha, "message_above": message_above, "alpha_change": alpha_change,
-                  "dura_approve": dura_approve, "dura_fail": dura_fail, "dura_win": dura_win, "why_yellow": why_yellow
+                  "dura_approve": dura_approve, "dura_fail": dura_fail, "dura_win": dura_win, "why_yellow": why_yellow,
+                  "grammar_check": grammar_check
 }
 
 
